f286a7d32fa5a130ff11b59fc02a70cd298059b5
Tomer Rosenfeld <tomerosenfeld007@gmail.com>
2015-12-10 19:25:13 +0200

    Updated example application

--- example/src/main/java/example/dialog/Test.java
+++ example/src/main/java/example/dialog/Test.java
@@ -3,6 +3,7 @@ package example.dialog;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import rosenpin.androidL.dialog.AndroidLDialog;
 
@@ -12,34 +13,47 @@ import rosenpin.androidL.dialog.AndroidLDialog;
  */
 public class Test extends Activity {
     AndroidLDialog androidLDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.example);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogExample();
+            }
+        });
+        dialogExample();
+    }
+
+    private void dialogExample() {
         androidLDialog = new AndroidLDialog.Builder(Test.this)
                 //settings title
                 .Title("Dialog Title!")
-                //settings message
+                        //settings message
                 .Message("Hi there! You are successfully using my library, android l dialog. \n My name is Tomer Rosenfeld I'm a 15 year old israeli kid who loves android, right now i'm typing stuff so you can see how a long message looks like in the dialog, try pressing the OK button to see the sexy animation.")
-                //adding positive (right) button
+                        //adding positive (right) button
                 .setPositiveButton("OK", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
-                    }
-                })
-                //adding negative (center) button
-                .setNegativeButton("CANCEL", new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
+                        Toast.makeText(Test.this, "You clicked OK", Toast.LENGTH_SHORT).show();
                         androidLDialog.hide();
                     }
                 })
-                //showing the dialog!
+                        //adding negative (center) button
+                .setNegativeButton("CANCEL", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(Test.this, "You clicked CANCEL", Toast.LENGTH_SHORT).show();
+                        androidLDialog.hide();
+                    }
+                })
+                        //showing the dialog!
                 .show();
 
-                //optional.. setting icon but it's not very android l like
-               //androidLDialog.setIcon(R.drawable.ic_launcher);
+        //optional.. setting icon but it's not very android l like
+        //androidLDialog.setIcon(R.drawable.ic_launcher);
         /**
          * Try out functions like
          *         androidLDialog.getTitle();
